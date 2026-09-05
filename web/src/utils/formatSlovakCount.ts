d8204b4f25f6b2d72cd5f885e56cd53c37672720
miro <miro.furinda@proton.me>
2026-09-05 19:26:49 +0200

    Slovak after-number words declension fix

--- web/src/utils/formatSlovakCount.ts
+++ web/src/utils/formatSlovakCount.ts
@@ -1,18 +1,16 @@
 export function getSlovakCountWord(base: string, count: number) {
   const absoluteCount = Math.abs(count);
-  const lastTwoDigits = absoluteCount % 100;
-  const lastDigit = absoluteCount % 10;
 
-  if (lastTwoDigits === 1 || (lastDigit === 1 && lastTwoDigits !== 11)) {
+  if (absoluteCount === 1) {
     return base;
   }
 
-  if (
-    lastDigit >= 2 &&
-    lastDigit <= 4 &&
-    (lastTwoDigits < 12 || lastTwoDigits > 14)
-  ) {
+  if (absoluteCount >=2 && absoluteCount <=4) {
     return `${base}y`;
+  }
+
+  if (absoluteCount === 0 || absoluteCount >= 5) {
+    return `${base}ov`;
   }
 
   return `${base}ov`;
